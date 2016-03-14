0ce6933ec37dcfd17c2ff4bedaf26825fb88f416
Michael EUSEBE <eusebe.michael@etu.unice.fr>
2016-03-14 06:24:58 +0100

    first commit

---
+++ elevator/src/Controler/Connector.java
@@ -0,0 +1,7 @@
+package Controler;
+
+/**
+ * Created by Michael on 14/03/2016.
+ */
+public class Connector {
+}
---
+++ elevator/src/Controler/Controler.java
@@ -0,0 +1,18 @@
+package Controler;
+
+import java.util.ArrayList;
+
+/**
+ * Created by Michael on 23/02/2016.
+ */
+public class Controler {
+
+    private ArrayList<Connector> connectors;
+
+    public Controler(){
+        connectors = new ArrayList<Connector>();
+    }
+
+
+}
+
